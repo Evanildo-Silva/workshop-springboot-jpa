1f82b0a4c4b6d86eb4e5f1e874fef381aa90cd9a
evanildo <evanildo.sf91@gmail.com>
2023-10-09 20:29:52 -0300

    feat: user repository interface

---
+++ src/main/java/tech/evanildodeveloper/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package tech.evanildodeveloper.course.repositories;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
