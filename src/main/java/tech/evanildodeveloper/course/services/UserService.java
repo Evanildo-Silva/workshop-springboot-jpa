4dbf360c80f20e130495e5b656ab49a7ef6ee2cf
evanildo <evanildo.sf91@gmail.com>
2023-10-09 21:45:11 -0300

    refactor: register UserService as spring component

--- src/main/java/tech/evanildodeveloper/course/services/UserService.java
+++ src/main/java/tech/evanildodeveloper/course/services/UserService.java
@@ -3,13 +3,16 @@ package tech.evanildodeveloper.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tech.evanildodeveloper.course.entities.User;
 import tech.evanildodeveloper.course.repositories.UserRepository;
 
+@Service // Annotation to register as a spring component
 public class UserService {
 
-    @Autowired // Annotation to perform dependency injection
+    @Autowired // Annotation to register UserService as spring component for dependency
+               // injection
     private UserRepository repository; // UserRepository dependency
 
     // Service method to run findAll on the repository
