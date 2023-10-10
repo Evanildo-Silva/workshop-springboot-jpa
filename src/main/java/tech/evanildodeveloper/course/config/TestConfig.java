58c49bcd83928ecc7b8bc11af933d15e2e636277
evanildo <evanildo.sf91@gmail.com>
2023-10-10 19:39:06 -0300

    refactor: Receive categories

--- src/main/java/tech/evanildodeveloper/course/config/TestConfig.java
+++ src/main/java/tech/evanildodeveloper/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import tech.evanildodeveloper.course.entities.Category;
 import tech.evanildodeveloper.course.entities.Order;
 import tech.evanildodeveloper.course.entities.User;
 import tech.evanildodeveloper.course.entities.enums.OrderStatus;
+import tech.evanildodeveloper.course.repositories.CategoryRepository;
 import tech.evanildodeveloper.course.repositories.OrderRepository;
 import tech.evanildodeveloper.course.repositories.UserRepository;
 
@@ -28,8 +30,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired // Annotation to perform dependency injection
     private OrderRepository orderRepository;
 
+    @Autowired // Annotation to perform dependency injection
+    private CategoryRepository categoryRepository;
+
     @Override // Override the CommandLineRunner interface method
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
