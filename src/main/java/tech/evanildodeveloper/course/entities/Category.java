2ae7ec4b8dc3bbd25d285a1aabc9b47ae114db4a
evanildo <evanildo.sf91@gmail.com>
2023-10-10 21:13:32 -0300

    refactor: Adds relationship with products

--- src/main/java/tech/evanildodeveloper/course/entities/Category.java
+++ src/main/java/tech/evanildodeveloper/course/entities/Category.java
@@ -1,11 +1,16 @@
 package tech.evanildodeveloper.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +22,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -40,6 +49,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
