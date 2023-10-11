b7fec9279f369505bec6143a702f7f680321ee0d
evanildo <evanildo.sf91@gmail.com>
2023-10-10 22:32:36 -0300

    refactor: add orderItems

--- src/main/java/tech/evanildodeveloper/course/config/TestConfig.java
+++ src/main/java/tech/evanildodeveloper/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import tech.evanildodeveloper.course.entities.Category;
 import tech.evanildodeveloper.course.entities.Order;
+import tech.evanildodeveloper.course.entities.OrderItem;
 import tech.evanildodeveloper.course.entities.Product;
 import tech.evanildodeveloper.course.entities.User;
 import tech.evanildodeveloper.course.entities.enums.OrderStatus;
 import tech.evanildodeveloper.course.repositories.CategoryRepository;
+import tech.evanildodeveloper.course.repositories.OrderItemRepository;
 import tech.evanildodeveloper.course.repositories.OrderRepository;
 import tech.evanildodeveloper.course.repositories.ProductRepository;
 import tech.evanildodeveloper.course.repositories.UserRepository;
@@ -37,6 +39,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired // Annotation to perform dependency injection
     private ProductRepository productRepository;
+
+    @Autowired // Annotation to perform dependency injection
+    private OrderItemRepository orderItemRepository;
 
     @Override // Override the CommandLineRunner interface method
     public void run(String... args) throws Exception {
@@ -74,5 +79,13 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
     }
 }
