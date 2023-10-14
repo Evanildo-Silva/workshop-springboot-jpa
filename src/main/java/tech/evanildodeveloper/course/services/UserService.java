0a77ea1cf0e51dfacc9c685e30a7264c991fc495
evanildo <evanildo.sf91@gmail.com>
2023-10-13 22:48:16 -0300

    feat: error handling delete method

--- src/main/java/tech/evanildodeveloper/course/services/UserService.java
+++ src/main/java/tech/evanildodeveloper/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import tech.evanildodeveloper.course.entities.User;
 import tech.evanildodeveloper.course.repositories.UserRepository;
+import tech.evanildodeveloper.course.services.exceptions.DatabaseException;
 import tech.evanildodeveloper.course.services.exceptions.ResourceNotFoundException;
 
 @Service // Annotation to register as a spring component
@@ -35,7 +38,13 @@ public class UserService {
 
     // Service to user delete method
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     // Service to user delete method
